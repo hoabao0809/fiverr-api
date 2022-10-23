401412cac019ee82b547c2eebce97bb71a7f0022
hoabao0809 <hoabao.0809@gmail.com>
2022-10-23 09:24:25 +0700

    fix deploy heroku

--- app.js
+++ app.js
@@ -27,14 +27,15 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization'
-  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
 // Routes
+app.use('/', (req, res, next) => {
+  res.send('Connected');
+  next();
+});
 app.use('/api', userRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api', authRoutes);
